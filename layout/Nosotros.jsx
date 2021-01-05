116192a2916ee68ed2ee20679ee4e9fe2ef00668
unknown <joaquinsantarcangelo@gmail.com>
2021-01-05 15:58:18 -0300

    Animations Fix

--- layout/Nosotros.jsx
+++ layout/Nosotros.jsx
@@ -36,7 +36,39 @@ const Nosotros = () => {
           Nosotros
         </motion.h1>
       </div>
-      <div className="content"></div>
+      <div className="content">
+        <motion.div
+          initial={{ opacity: "0%" }}
+          transition={{ when: "beforeChildren", staggerChildren: 1 }}
+          exit={{
+            width: "0%",
+            transition: { delay: 1, duration: 1, when: "afterChildren" },
+          }}
+          animate={{ opacity: "100%" }}
+          className="card"
+        >
+          <motion.h4
+            initial={{ opacity: 0 }}
+            transition={{ delay: 1 }}
+            exit={{ opacity: 0, transition: { delay: 0 } }}
+            animate={{ opacity: 1 }}
+          >
+            Somos una empresa familiar que inició en el rubro en 1936, a lo
+            largo de los años fuimos especializándonos en distintos segmentos
+            dentro del rubro, hasta hoy en dia ser especialistas en los{" "}
+            <span>panificados de papa</span>.
+          </motion.h4>
+          <motion.h4
+            initial={{ opacity: 0 }}
+            transition={{ delay: 1 }}
+            exit={{ opacity: 0, transition: { delay: 0 } }}
+            animate={{ opacity: 1 }}
+          >
+            Nuestro objetivo como empresa, es poder brindar la mejor calidad de
+            panificados, en la mesa de cada familia.
+          </motion.h4>
+        </motion.div>
+      </div>
     </div>
   );
 };
